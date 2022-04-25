1a1c4171463e08c249153a1d7908b9f1b3fe4c1e
VoltXd <47433478+VoltXd@users.noreply.github.com>
2022-04-25 21:23:43 +0200

    Ajout thread connexion usb

--- PWMtester.py
+++ PWMtester.py
@@ -6,12 +6,35 @@ Created on Sat Apr 16 10:38:26 2022
 """
 
 from tkinter import *
-import PIL 
+import threading
+import time
 import serial
+import serial.tools.list_ports
 import connectionSPI
 
-sp = serial.Serial('COM4', 115200)
+programEnded = False
+
+def threadSerialPort(serialPort, tkWindow):
+    while not programEnded:
+        comPortList = [p[0] for p in list(serial.tools.list_ports.comports())]
+        if (not serialPort.is_open):
+            for port in comPortList:
+                try:
+                    serialPort.port = port
+                    serialPort.open()
+                    tkWindow.title("PWM tester - connected [{}]".format(port))
+                except serial.SerialException:
+                    print("Unable to open port " + port)
+        elif (serialPort.port not in comPortList):
+            serialPort.close()
+            tkWindow.title("PWM tester - connexion pending")
+        time.sleep(0.1)
+    
+sp = serial.Serial()
+sp.baudrate = 115200
+
 comm = connectionSPI.SpiProtocolAutonomousCar()
+
 
 pwmProp = 1500
 pwmDir = 1150
@@ -24,26 +47,33 @@ dirMin = 1300
 
 def slideProp(var):
     pwmProp = propulsion.get()
-    sp.write(comm.encodeMessage(pwmProp, pwmDir))
+    pwmDir = direction.get()
+    if sp.is_open:
+        sp.write(comm.encodeMessage(pwmProp, pwmDir))
     
 def slideDir(var):
     pwmDir = direction.get()
-    sp.write(comm.encodeMessage(pwmProp, pwmDir))
+    pwmProp = propulsion.get()
+    if sp.is_open:
+        sp.write(comm.encodeMessage(pwmProp, pwmDir))
     
 def reset():
     pwmProp = 1500
     pwmDir = 1150
-    sp.write(comm.encodeMessage(pwmProp, pwmDir))
+    if sp.is_open:
+        sp.write(comm.encodeMessage(pwmProp, pwmDir))
     propulsion.set(pwmProp)
     direction.set(pwmDir)
 
 width = 1500
-height = 300
+height = 150
 
 root = Tk()
-root.title("PWM tester")
-root.iconbitmap("")
+root.title("PWM tester - connexion pending")
 root.geometry(str(width) + "x" + str(height))
+
+threadSPManager = threading.Thread(target=threadSerialPort, args=(sp, root, ))
+threadSPManager.start()
 
 propulsion = Scale(root, from_=propMin, to=propMax, orient=HORIZONTAL, resolution=1, length=width*0.9, label='Propulsion', command=slideProp)
 propulsion.pack()
@@ -58,4 +88,7 @@ myButton = Button(root, command=reset, text='Reset').pack()
     
 
 root.mainloop()
-sp.close()
+if sp.isOpen():
+    sp.close()
+programEnded = True    
+threadSPManager.join()
