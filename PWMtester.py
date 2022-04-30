6f832ac9d1deb852e9a827c1b178430bbe19b976
VoltXd <pierrealexandre.peyronnet@gmail.com>
2022-04-30 22:34:34 +0200

    Amélioration testeur
    - Ajout de combobox pour port série et protocole
    - Ajout terminal
    
    A faire : 
    - Ajouter oscillo ?
    - Ajouter mesures capteur vitesse ?

--- PWMtester.py
+++ PWMtester.py
@@ -6,6 +6,7 @@ Created on Sat Apr 16 10:38:26 2022
 """
 
 from tkinter import *
+from tkinter import ttk
 import threading
 import time
 import serial
@@ -15,7 +16,44 @@ import connectionSPI
 #boolean used to stop thread at the end
 programEnded = False
 
-def threadSerialPort(serialPort, tkWindow):
+#Serial port declaration
+sp = serial.Serial()
+sp.baudrate = 115200
+
+#Protocol object declaration (class + file name suck)
+comm = connectionSPI.SpiProtocolAutonomousCar()
+
+#Default values
+pwmProp = 1500
+pwmDir = 1150
+speed0 = 0
+
+#Limit values
+propMax = 2000
+propMin = 1000
+dirMax = 1000
+dirMin = 1300
+speedMax = 8
+speedMin = 0
+
+#Variable for Terminal
+terminalBufferSize = 128
+
+#Functions
+def threadUpdateTerminal(serialPort, tkTerminal, stringBuffer):
+    while not programEnded:
+        comPortList = [p[0] for p in list(serial.tools.list_ports.comports())]
+        if serialPort.is_open and serialPort.port in comPortList:
+            stringBuffer += serialPort.read_all().decode("ASCII")
+            if len(stringBuffer) > terminalBufferSize:
+                stringBuffer = stringBuffer[(len(stringBuffer) - terminalBufferSize):]
+            if bool(tkTerminal.winfo_exists()):
+                tkTerminal.delete(1.0, END)
+                tkTerminal.insert(END, stringBuffer)
+        time.sleep(0.1)
+    
+
+def threadSerialPort(serialPort, tkWindow, portCB):
     """
     Enables to connect and disconnec serial ports while
     the program is running.
@@ -32,44 +70,49 @@ def threadSerialPort(serialPort, tkWindow):
     None.
 
     """
+    #The program crashes when it tries to acces destroyed widgets,
+    #save previous ports to limit their acces.
+    previousPorts = []
+    
+    #Infinite loop
     while not programEnded:
-        #Check every ports connected
+        #Check every ports connected, save them if they changed
         comPortList = [p[0] for p in list(serial.tools.list_ports.comports())]
+        if not comPortList == previousPorts:
+            #Change detected, modify combobox
+            if len(comPortList) == 0:
+                portCB['values'] = ["No port"]
+                portCB.set("No port")
+            else:
+                portCB['values'] = comPortList
+        previousPorts = comPortList.copy()
         
         #If nothing is connected, try to connect to ports until it's connected
-        if (not serialPort.is_open):
+        if (not serialPort.is_open):    
             for port in comPortList:
                 try:
                     serialPort.port = port
                     serialPort.open()
                     tkWindow.title("PWM tester - connected [{}]".format(port))
+                    portCB.set(port)
                 except serial.SerialException:
                     print("Unable to open port " + port)
+        #Else, if the selected port is not the connected one, change it
+        elif portCB.get() not in [serialPort.port, "No port"]:
+            port = portCB.get()
+            try:
+                serialPort.close()
+                serialPort.port = port
+                serialPort.open()
+                tkWindow.title("PWM tester - connected [{}]".format(port))
+            except serial.SerialException:
+                print("Unable to open port " + port)
         #Else, if the connection has ended, close the port
         elif (serialPort.port not in comPortList):
             serialPort.close()
             tkWindow.title("PWM tester - connexion pending")
         #Wait to avoid spam
         time.sleep(0.1)
-    
-#Serial port declaration
-sp = serial.Serial()
-sp.baudrate = 115200
-
-#Protocol declaration (name sucks + should not be a class now ?)
-comm = connectionSPI.SpiProtocolAutonomousCar()
-
-
-#Default values
-pwmProp = 1500
-pwmDir = 1150
-
-#Limit values
-propMax = 2000
-propMin = 1000
-dirMax = 1000
-dirMin = 1300
-
 
 def slideProp(var):
     """
@@ -89,7 +132,8 @@ def slideProp(var):
     if sp.is_open:
         pwmProp = propulsion.get()
         pwmDir = direction.get()
-        sp.write(comm.encodeMessage(pwmProp, pwmDir))
+        payload = (pwmProp, pwmDir)
+        sp.write(comm.encodeMessage(payload))
     
 def slideDir(var):
     """
@@ -109,7 +153,8 @@ def slideDir(var):
     if sp.is_open:
         pwmDir = direction.get()
         pwmProp = propulsion.get()
-        sp.write(comm.encodeMessage(pwmProp, pwmDir))
+        payload = (pwmProp, pwmDir)
+        sp.write(comm.encodeMessage(payload))
     
 def reset():
     """
@@ -121,40 +166,119 @@ def reset():
     None.
 
     """
-    pwmProp = 1500
+    if comm.protocol == "PWM":
+        pwmProp = 1500
+    elif comm.protocol == "ASSERVISSEMENT":
+        pwmProp = 0
     pwmDir = 1150
     if sp.is_open:
-        sp.write(comm.encodeMessage(pwmProp, pwmDir))
+        payload = (pwmProp, pwmDir)
+        sp.write(comm.encodeMessage(payload))
     propulsion.set(pwmProp)
     direction.set(pwmDir)
+    
+def onProtocolChange(evt):
+    """
+    
+
+    Parameters
+    ----------
+    evt : Tkinter.event
+        Tells many things.
+
+    Returns
+    -------
+    None.
+
+    """
+    protocol = protocolCB.get()
+    comm.setProtocol(protocol)
+    if protocol == "PWM":
+        propulsion.configure(to=propMax, from_=propMin, resolution=1, label='Propulsion')
+        propulsion.set(pwmProp)
+    elif protocol == "ASSERVISSEMENT":
+        propulsion.configure(to=speedMax, from_=speedMin, resolution=0.001, label='Consigne de vitesse')
+        propulsion.set(speed0)
+    propulsion.pack()
+
+    
 
 #Window size
-width = 1500
-height = 150
+width = 700
+height = 350
 
 #Window Init.
 root = Tk()
 root.title("PWM tester - connexion pending")
 root.geometry(str(width) + "x" + str(height))
+root.resizable(width=False, height=False)
 
-#Thread Init.
-threadSPManager = threading.Thread(target=threadSerialPort, args=(sp, root, ))
-threadSPManager.start()
+#Frames
+lfCommand = LabelFrame(root, text="Commandes", padx=10, pady=10)
+lfCommand.pack(side=BOTTOM, fill="both", expand="yes")
+
+lfSettings = LabelFrame(root, text="Paramètres", padx=10, pady=10)
+lfSettings.pack(anchor='nw', side=LEFT, fill="both", expand="yes")
+
+lfTerminal = LabelFrame(root, text="Terminal", padx=10, pady=10)
+lfTerminal.pack(anchor='ne', side=RIGHT, fill="both", expand="yes")
+
+fSliders = Frame(lfCommand, relief=GROOVE, borderwidth=2)
+fSliders.pack(padx = 10, pady=10)
+
+fPort = Frame(lfSettings, relief=GROOVE, borderwidth=0)
+fPort.pack(side=LEFT, anchor="nw", padx = 10, pady=0)
+
+fProtocol = Frame(lfSettings, relief=GROOVE, borderwidth=0)
+fProtocol.pack(side=RIGHT, anchor="ne", padx = 10, pady=0)
 
 #Propulsion slider 
-propulsion = Scale(root, from_=propMin, to=propMax, orient=HORIZONTAL, resolution=1, length=width*0.9, label='Propulsion', command=slideProp)
+propulsion = Scale(fSliders, from_=propMin, to=propMax, orient=HORIZONTAL, resolution=1, length=width*0.95, label='Propulsion', command=slideProp)
 propulsion.pack()
 propulsion.set(pwmProp)
 
 #Direction slider
-direction = Scale(root, from_=dirMin, to=dirMax, orient=HORIZONTAL, resolution=1, length=width*0.9, label='Direction', command=slideDir)
+direction = Scale(fSliders, from_=dirMin, to=dirMax, orient=HORIZONTAL, resolution=1, length=width*0.95, label='Direction', command=slideDir)
 direction.pack()
 direction.set(pwmDir)
 
 #Reset button
-resetButton = Button(root, command=reset, text='Reset')
+resetButton = Button(lfCommand, command=reset, text='Reset')
 resetButton.pack()
-    
+
+#COM PORT label
+comportL = Label(fPort, text="Port", padx = 10, pady=10)
+comportL.pack(side=TOP, anchor='nw')
+
+#COM PORT combobox
+comportCB = ttk.Combobox(fPort, state="readonly", values=["No port"])
+comportCB.set("No port")
+comportCB.pack(side=LEFT, anchor="nw")
+
+
+#Protocol label
+protocolL = Label(fProtocol, text="Protocole", padx = 10, pady=10)
+protocolL.pack(anchor='nw')
+
+#Protocol combobox
+protocolCB = ttk.Combobox(fProtocol, state="readonly", values=["PWM", "ASSERVISSEMENT"])
+protocolCB.set("PWM")
+protocolCB.bind("<<ComboboxSelected>>", onProtocolChange)
+protocolCB.pack(side=LEFT, anchor="nw")
+
+#Terminal label
+terminal = Text(lfTerminal, padx=10, pady=10, state='normal')
+terminal.pack()
+
+#Timer init.
+terminalBuffer = ""
+terminalUpdateThread = threading.Thread(target=threadUpdateTerminal, args=(sp, terminal, terminalBuffer, ))
+terminalUpdateThread.start()
+
+#Thread Init.
+threadSPManager = threading.Thread(target=threadSerialPort, args=(sp, root, comportCB, ))
+threadSPManager.start()
+
 #infinite loop
 root.mainloop()
 
@@ -163,3 +287,4 @@ if sp.isOpen():
     sp.close()          #Closing serial port
 programEnded = True     #Closing thread
 threadSPManager.join()
+terminalUpdateThread.join()
--- connectionSPI.py
+++ connectionSPI.py
@@ -8,25 +8,102 @@ Created on Fri Feb 19 09:32:47 2021
 import struct
 
 class SpiProtocolAutonomousCar():
-    def __init__(self):
+    """Class used for communication protocol"""
+    PROTOCOLS = ("PWM", "ASSERVISSEMENT")
+    
+    def __init__(self, protocol='PWM'):
+        """
+        
+
+        Parameters
+        ----------
+        protocol : str, optional
+            Protocol used. The default is 'PWM'.
+
+        Returns
+        -------
+        None.
+
+        """
+        if protocol in self.PROTOCOLS:
+            self.protocol = protocol
+        else:
+            self.protocol = "PWM"
         return
     
     #***************Frame Encoding***************
     #pwmPropulsion (int 2 Bytes) : Pwm propulsion value 
     #pwmDirection (int 2 Bytes) : Pwm direction value 
-    def encodeMessage(self, pwmPropulsion, pwmDirection):
-        msg = b'\xFF'
-        msg += pwmPropulsion.to_bytes(2, 'big')
-        msg += pwmDirection.to_bytes(2, 'big')
-        msg += self.calculateChecksum(msg).to_bytes(1, 'big')
+    def encodeMessage(self, payload):
+        """
+        Encode a message according to the protocol selected
+
+        Parameters
+        ----------
+        payload : tuple
+            Contains the variables to transmit.
+
+        Returns
+        -------
+        msg : bytes
+            The message to transmit.
+
+        """
+        if self.protocol == "PWM":
+            pwmPropulsion = payload[0]
+            pwmDirection = payload[1]
+            msg = b'\xFF'
+            msg += pwmPropulsion.to_bytes(2, 'big')
+            msg += pwmDirection.to_bytes(2, 'big')
+            msg += self.calculateChecksum(msg).to_bytes(1, 'big')
+        elif self.protocol == "ASSERVISSEMENT":
+            speedCommand = payload[0]
+            pwmDirection = payload[1]
+            msg = b'\xFF'
+            msg += struct.pack('f', speedCommand)
+            msg += pwmDirection.to_bytes(2, 'big')
+            msg += self.calculateChecksum(msg).to_bytes(1, 'big')            
         
         #Envoie de la trame
         #self.sp.write(msg)
         #for b in msg:
         #    print(b)
         return msg
+    
+    def setProtocol(self, protocol):
+        """
+        Sets the protocol
+
+        Parameters
+        ----------
+        protocol : str
+            Name of the protocol to use.
+
+        Returns
+        -------
+        None.
+
+        """
+        if protocol in self.PROTOCOLS:
+            self.protocol = protocol
+        return
         
+    
     def calculateChecksum(self, msg):
+        """
+        Calculate the checksum of the frame, using 8 bits XOR
+
+        Parameters
+        ----------
+        msg : bytes
+            Message without checksum.
+
+        Returns
+        -------
+        checksum : bytes
+            Result of the 8XOR.
+
+        """
         checksum = 0
         for b in msg:
             checksum ^= b
